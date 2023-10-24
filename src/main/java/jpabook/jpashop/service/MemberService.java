c1116535fc2529eb63a3f5c0752e33408aa92101
Kelly Park <sehyun.kelly.park@gmail.com>
2023-10-24 14:25:55 -0700

    Member repository, service, test completed

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * Sign up
+     */
+    @Transactional
+    public Long join(Member member) {
+       validateDuplicateMember(member); //check for duplicated members
+       memberRepository.save(member);
+       return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("Member information already exists");
+        }
+    }
+
+    /**
+     * Search all members
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
